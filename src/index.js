b76a6101c3ec87a2d55cdff6060f6eff81047b75
Demetrios <jkopatsis@gmail.com>
2023-03-31 15:36:39 -0700

    ship factory created and tested

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-document.querySelector('.test').textContent = 'ree';
+const Ship = (pr_name, pr_length) => {
+    const name = pr_name;
+    const length = pr_length;
+    let squares = {};
+    let sunk = false;
+
+    const hit = (sq) => {
+        if (sq in squares){
+            squares[sq] = "hit";
+        }
+        if(sinkCheck()){
+            return "sunk";
+        } else{
+            return "not sunk";
+        }
+    }
+
+    const sinkCheck = () => {
+        for (const key in squares){
+            if (squares[key]==="unhit") return false;
+        }
+        sunk = true;
+        return true;
+    }
+
+    const squareCreate = (posArray) => {
+        for(const position of posArray){
+            squares[position] = "unhit";
+        }
+    }
+
+    const isSunk = () => {
+        return sunk;
+    }
+
+    return {name, length, squares, sunk, hit, squareCreate, isSunk}
+}
