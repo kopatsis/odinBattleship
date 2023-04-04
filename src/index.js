5725dbb641e338d8910873ce67300ce297349a9c
Demetrios <jkopatsis@gmail.com>
2023-04-04 14:35:20 -0700

    overall gameplay functionality existant, needs debugging

--- src/index.js
+++ src/index.js
@@ -11,22 +11,54 @@ let DOMob = overallDOM();
 const left = document.querySelectorAll('.left');
 const right = document.querySelectorAll('.right');
 
-
-const gameRound = (e) => {
-    const result = human.mainMover(parseInt(e.target.classList[1]));
-    console.log(result);
+const gameOverFunc = () => {
+    right.forEach(sq => sq.onclick = '');
+    document.querySelector('.rightboard').style.cursor = "default";
+    document.querySelector('.start').onclick = reset();
 }
 
 
-const gamePlay = () => {
-    right.forEach(sq => sq.onclick = gameRound);
-    document.querySelector('.leftboard').style.cursor = "default";
-    document.querySelector('.rightboard').style.cursor = "pointer";
-    // let inProgress = true;
-    // while(inProgress){
-        // const result = human.mainMover();
-        // console.log(result);
-    // }
+const gameRound = (e) => {
+    const result = human.mainMover(parseInt(e.target.classList[1]));
+    let res1 = "";
+    let res2 = "";
+    if(result[0][1]==="hit"){
+        right[result[0][0]].classList.add('ship');
+        right[result[0][0]].classList.add('hit');
+        res1 += "Your strike was a hit";
+        if(result[0][2] !== null){
+            res1 += `, you sunk your opponent's ${result[0][2]}. `;
+            if(result[0][3]==="Gameover"){
+                res1 += "You won the game!";
+            }
+        }
+    } else {
+        right[result[0][0]].classList.add('miss');
+        res1 += "Your strike was a miss";
+    }
+    document.querySelector('.result1').textContent = res1;
+    if(result[0][3]==="Gameover"){
+        document.querySelector('.result2').textContent = '';
+        gameOverFunc();
+    } else {
+        if(result[1][1]==="hit"){
+            left[result[1][0]].classList.add('hit');
+            res2 += "Your opponent's strike was a hit";
+            if(result[1][2] !== null){
+                res2 += `, your opponent sunk your ${result[1][2]}. `;
+                if(result[1][3]==="Gameover"){
+                    res2 += "You lost the game :(";
+                }
+            }
+        } else {
+            left[result[1][0]].classList.add('miss');
+            res2 += "Your opponent's strike was a miss";
+        }
+        document.querySelector('.result2').textContent = res2;
+        if(result[1][3]==="Gameover"){
+            gameOverFunc();
+        }
+    }
 }
 
 const gameStart = () => {
@@ -39,7 +71,9 @@ const gameStart = () => {
         human.board.shipPlacer("Submarine", positions[3]);
         human.board.shipPlacer("Destroyer", positions[4]);
         document.querySelector('.instruct').textContent = "Place your first hit on your opponent's board";
-        gamePlay();
+        right.forEach(sq => sq.onclick = gameRound);
+        document.querySelector('.leftboard').style.cursor = "default";
+        document.querySelector('.rightboard').style.cursor = "pointer";
     }
 }
 
